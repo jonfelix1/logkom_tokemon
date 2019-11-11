464cf3abf139795f9cc57c8d8f0ea01e2ca58666
Jon Felix <jonfelix1@gmail.com>
2019-11-11 12:22:04 +0700

    init

---
+++ tokemon.pl
@@ -0,0 +1,43 @@
+
+
+
+
+start() :-
+
+    write('Welcome to Tokemon'),
+    menu().
+
+menu() :-
+    read(Choice),
+    option(Choice).
+
+
+%movement
+option(w) :-
+    
+    menu().
+
+option(a) :-
+
+    menu().
+
+option(s) :-
+
+    menu().
+
+option(d) :-
+
+    menu().
+
+option(Status) :-
+
+    menu().
+
+option(Map) :-
+
+    menu().
+
+fightmenu() :-
+    read(Choice),
+    foption(Choice).
+
